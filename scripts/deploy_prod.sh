f9481c66a9bac617ef888b222a06baf909b66a8e
Fabian Schweizer <fa.schweizer@gmail.com>
2014-11-30 22:22:41 +0100

    Added a deploy script for deploying on the production server

---
+++ scripts/deploy_prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Usage: deploy_prod.ssh USER@HOST
+echo "Updating files on server"
+ssh -t $1 "
+    cd /srv/npl.ch
+    git pull --rebase --stat
+    sudo chown -R www-data:www-data .
+"
