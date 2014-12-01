37dc16300ea1369acd5a57fbb9ded7bc5ef7dfbb
Fabian Schweizer <fa.schweizer@gmail.com>
2014-12-01 07:23:18 +0100

    First draft of news controller in admin

---
+++ admin/controllers/NewsController.php
@@ -0,0 +1,41 @@
+<?php
+
+class NewsController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        return $this->_helper->redirector('list');
+    }
+
+    public function listAction() {
+        Zend_Paginator::setDefaultItemCountPerPage(5);
+
+        $newsMapper = new Application_Model_Mapper_NewsMapper();
+        $news = $newsMapper->fetchAll();
+        $paginator = Zend_Paginator::factory($news);
+
+        $page = $this->getRequest()->getParam('page', 1);
+        if (!is_numeric($page) || $page < 1) {
+            $page = 1;
+        } else {
+            $page = (int) $page;
+        }
+
+        $paginator->setCurrentPageNumber($page);
+        Zend_Paginator::setDefaultScrollingStyle('Sliding');
+        Zend_View_Helper_PaginationControl::setDefaultViewPartial(
+            'partials/paginator/controls.phtml'
+        );
+
+        $this->view->paginator = $paginator;
+
+        return;
+    }
+}
+
