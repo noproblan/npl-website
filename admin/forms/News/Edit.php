0571391ea1ca76c3aaeb7dd1c209ccd328ff5a76
Fabian Schweizer <fa.schweizer@gmail.com>
2014-12-20 10:13:32 +0100

    2 - Made list html5 conform

--- admin/forms/News/Edit.php
+++ admin/forms/News/Edit.php
@@ -9,5 +9,16 @@ class Admin_Form_News_Edit extends Admin_Form_News_New
 		$this->addElement($id);
         parent::init();
         $this->setAttrib('id', 'edit_entry');
+
+        $this->removeElement('submit');
+        $submit = new Zend_Form_Element_Submit(
+            'submit',
+            array(
+                'required' => false,
+                'ignore' => true,
+                'label' => 'Speichern',
+            )
+        );
+        $this->addElement($submit);
 	}
 }
