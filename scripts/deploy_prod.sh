d671c34e4c080e860835169b06fdfaabd15ceeec
Josua Schmid <jschmid@fastmail.net>
2014-12-21 15:38:15 +0100

    Renamed website folder

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -2,7 +2,7 @@
 # Usage: deploy_prod.ssh USER@HOST
 echo "Updating files on server"
 ssh -t $1 "
-    cd /srv/npl.ch
+    cd /srv/noproblan.ch
     git pull --rebase --stat
     sudo chown -R www-data:www-data .
 "
